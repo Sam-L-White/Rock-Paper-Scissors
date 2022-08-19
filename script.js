ec773dd166ff3650c7f03be7aa9992e0f9045a0f
Sam White <samuelwhite98@outlook.com>
2022-08-19 19:35:21 +0100

    Add playGame function to resolve winner

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 function getComputerChoice(){
     result = Math.floor(Math.random() * 3)
     switch (result){
-        case result = 0:
-            return "Rock"
-        case result = 1:
-            return "Paper"
-        case result = 2:
-            return "Scissors"
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection){
+    loss = "You lose! Try Again."
+    win = "You win! Well done."
+    draw = "It's a draw!"
+    switch (playerSelection.toLowerCase()){ 
+        case "rock":
+            switch(computerSelection){
+                case "paper":
+                    return loss
+                case "rock":
+                    return draw
+                case "scissors":
+                    return win
+            }
+        case "paper":
+            switch(computerSelection){
+                case "scissors":
+                    return loss
+                case "paper":
+                    return draw
+                case "rock":
+                    return win
+            }
+        case "scissors":
+            switch(computerSelection){
+                case "rock":
+                    return loss
+                case "scissors":
+                    return draw
+                case "paper":
+                    return win
+            }
+    }        
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice()
+console.log(playerSelection)
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
